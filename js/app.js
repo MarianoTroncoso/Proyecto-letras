0001ef327caf9ce0dc42eada999c18fb1e9b9d86
Mariano <petotronco@gmail.com>
2020-04-09 18:16:56 -0300

    control de campos vacios en el formulario

--- js/app.js
+++ js/app.js
@@ -2,4 +2,23 @@
 
 import * as UI from './interfaz.js';
 
-console.log(UI)
+UI.formularioBuscar.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // en esta parte se recomienda exportar los datos que son estaticos
+    // NO importamos las variables directamente
+    const artista = document.querySelector('#artista').value,
+        cancion = document.querySelector('#cancion').value;
+
+    // control de campos vacios
+    if(artista === '' || cancion === ''){
+        UI.divMensajes.innerHTML = 'Error... Todos los campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+        setTimeout(() => {
+            UI.divMensajes.innerHTML = '';
+            UI.divMensajes.classList.remove('error');
+        }, 3000);
+    } else {
+        // formulario se completo, realizar consulta en API
+    }
+})
